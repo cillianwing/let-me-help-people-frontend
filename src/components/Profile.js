06e8a8211f4cc6c422a13742bfb61fb98c4aaaea
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-08-01 02:56:47 -0500

    Created the basic React frontend portion of my project which includes functioning user authentication, a homepage with a side navbar and dashboard.

---
+++ src/components/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = (props) => {
+  return (
+    <div>
+      Testing the profile
+    </div>
+  )
+}
+
+export default Profile;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-import { Provider } from 'react-redux'
-import store from './store'
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
